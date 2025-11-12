13c0bb6cfc34662bc96d7a861836dccb77067ef3
Nipun Lakshan <nipunlakshan81@gmail.com>
2025-11-12 12:56:58 +0530

    Practice 01

---
+++ 01. Day 01/Practice01.m
@@ -0,0 +1,15 @@
+clear all
+close all
+clc
+
+%comment a single line
+x = linspace(0, 10, 200);
+y=sin(x);
+plot(x,y);
+
+
+
+
+disp("I' m Nipun");%this is a string_fill_char
+disp("Hello World");
+disp("I'm going to learn octave");
